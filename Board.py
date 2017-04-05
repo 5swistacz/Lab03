da03d8868562b2ac5a0be71be57691ba7816fa46
5swistacz <5swistacz@fis.agh.edu.pl>
2017-04-05 14:12:21 +0200

    second init

--- Board.py
+++ Board.py
@@ -1,5 +1,5 @@
 import random
-import copy
+from Validator import InputDataValidator
 
 class Board:
     def __init__(self):
@@ -18,6 +18,8 @@ class Board:
         self.corners = [0, 2, 6, 8]
         self.sides = [1, 3, 5, 7]
         self.middle = 4
+        self.list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self._validator = InputDataValidator()
 
     def print_board(self):
         print(self.board[7], self.board[8], self.board[9])
@@ -42,13 +44,9 @@ class Board:
         return False
 
     def get_bot_move(self, bot_marker):
-        for i in range(1, len(self.board)):
-            # board_copy = copy.deepcopy(self.board)
-            if self.is_space_free(self.board, i):
-                return i
-                # self.make_move(i, bot_marker)
-                # if self.is_winner(bot_marker):
-                #     return i
+        move = self.choose_random_move(self.list)
+        # if self.is_space_free(self.board, move):
+        return move
 
     def is_space_free(self, board, index):
         "checks for free space of the board"
@@ -69,13 +67,14 @@ class Board:
         for index in move_list:
             if self.is_space_free(self.board, index):
                 possible_winning_moves.append(index)
-                if len(possible_winning_moves) != 0:
-                    return random.choice(possible_winning_moves)
-                else:
-                    return None
+        if len(possible_winning_moves) != 0:
+            return random.choice(possible_winning_moves)
+        else:
+            return None
 
     def get_player_move(self):
-        move = int(input("Pick a spot to move: (1-9) "))
+        move = input("Pick a spot to move: (1-9) ")
+        move = self._validator.validate_if_number_is_int(move)
         while move not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not self.is_space_free(self.board, move ):
             move = int(input("Invalid move. Please try again: (1-9) "))
         return move
